af121e5d68fe062068e692a1f003f50370938dbd
SiHarv <harvey.casane@evsu.edu.ph>
2024-12-12 03:16:32 +0800

    made some design changes in userREGISTER and PROFILE view

--- views/user/userPROFILE.php
+++ views/user/userPROFILE.php
@@ -75,7 +75,7 @@ if (isset($_POST['update'])) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>Profile</title>
     <link rel="stylesheet" href="../../assets/css/bootstrap.css">
     <link rel="stylesheet" href="../../assets/css/userPROFILE.css">
